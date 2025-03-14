6241d6362a162b05fd8654e4f29d10f95c7f77df
CharredLee <gcc2hq@virginia.edu>
2025-03-13 21:50:10 -0400

    revert breaking change in nvf conf

--- modules/home/nvim/languages/lean.nix
+++ modules/home/nvim/languages/lean.nix
@@ -7,27 +7,6 @@
           mappings = true
         }
       '';
-      # taken from Julian's dotfiles: https://github.com/Julian/dotfiles
-      after = ''
-        vim.wo.signcolumn = 'yes'
-
-        vim.bo.textwidth = 100
-
-        vim.keymap.set('n', '<LocalLeader>g', function()
-          require'telescope.builtin'.live_grep{
-            glob_pattern = '*.lean',
-            path_display = { 'tail' },
-            search_dirs = require('lean').current_search_paths()
-          }
-        end, { buffer = true, desc = 'live grep the Lean search path.' })
-        vim.keymap.set('n', '<LocalLeader>l', function()
-          require'telescope'.extensions.loogle.loogle{}
-        end, { buffer = true, desc = 'search Loogle via Telescope' })
-
-        vim.keymap.set('i', '<C-;>', function()
-          require'telescope'.extensions.lean_abbreviations.lean_abbreviations{}
-        end, { buffer = true, desc = 'insert a unicode abbreviation' })
-      '';
     };
   };
 }
