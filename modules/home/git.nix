6fe3023db348d76734280bf1f1e828ab26fb79cb
CharredLee <gcc2hq@virginia.edu>
2025-03-19 03:02:13 -0400

    add direnv to global gitignore

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -1,11 +1,13 @@
-{ host, ... }:
-let
+{host, ...}: let
   inherit (import ../../hosts/${host}/variables.nix) gitUsername gitEmail;
-in
-{
+in {
   programs.git = {
     enable = true;
     userName = "${gitUsername}";
     userEmail = "${gitEmail}";
+    ignores = [
+      "*/.direnv"
+      "*/.envrc"
+    ];
   };
 }
