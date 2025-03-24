bb8d18bbcf17a1823fd7d4f47538d371d8abf0b1
CharredLee <gcc2hq@virginia.edu>
2025-03-24 01:01:51 -0400

    update vesktop css

--- modules/home/discord.nix
+++ modules/home/discord.nix
@@ -1,5 +1,23 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  config,
+  ...
+}: {
   home.packages = with pkgs; [
     vesktop
   ];
+
+  home.file = {
+    ".config/vesktop/settings/quickCss.css" = {
+      # source = config.lib.file.mkOutOfStoreSymlink ./quickCss.css;
+      text = ''
+        :root {
+          font-family: var(--font, '${config.stylix.fonts.monospace.name}'), '${config.stylix.fonts.monospace.name}';
+          font-primary: var(--font, '${config.stylix.fonts.monospace.name}'), '${config.stylix.fonts.monospace.name}';
+          font-display: var(--font, '${config.stylix.fonts.monospace.name}'), '${config.stylix.fonts.monospace.name}';
+        }
+      '';
+      executable = false;
+    };
+  };
 }
