4d6df9bb8d9c2e682683ebb0ee81f6314271d691
CharredLee <gcc2hq@virginia.edu>
2025-03-25 18:18:17 -0400

    add laptop2 host

---
+++ hosts/laptop2/variables.nix
@@ -0,0 +1,31 @@
+{
+  # Git Configuration ( For Pulling Software Repos )
+  gitUsername = "CharredLee";
+  gitEmail = "gcc2hq@virginia.edu";
+
+  # Hyprland Settings
+  extraMonitorSettings = "
+    monitor = eDP-1, 1920x1080@60, auto, auto;
+  ";
+  defaultWallpaper = "hollow-knight.png";
+
+  # Waybar Settings
+  clock24h = true;
+
+  # Program Options
+  browser = "zen"; # Set Default Browser (google-chrome-stable for google-chrome)
+  terminal = "kitty"; # Set Default System Terminal
+  keyboardLayout = "us";
+  consoleKeyMap = "us";
+
+  editor = "nvim";
+  EDITOR = "nvim";
+  VISUAL = "nvim";
+
+  # For Nvidia Prime support
+  intelID = "PCI:1:0:0";
+  nvidiaID = "PCI:0:2:0";
+
+  # Enable NFS
+  enableNFS = true;
+}
