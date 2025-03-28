1ffeb705675c8627318d0aa9b41453038e386875
CharredLee <gcc2hq@virginia.edu>
2025-03-27 22:25:43 -0400

    move waybar style back into default.nix

--- modules/home/waybar/default.nix
+++ modules/home/waybar/default.nix
@@ -1,6 +1,7 @@
 {
   pkgs,
   config,
+  lib,
   host,
   ...
 }: let
@@ -199,6 +200,338 @@ in {
         };
       }
     ];
-    style = builtins.readFile ./style.css;
+    # style = builtins.readFile ./style.css;
+    style = lib.concatStrings [
+      ''
+        * {
+          font-family: "JetBrainsMono Nerd Font Mono";
+          font-weight: bold;
+          min-height: 0;
+          /* set font-size to 100% if font scaling is set to 1.00 using nwg-look */
+          font-size: 97%;
+          font-feature-settings: '"zero", "ss01", "ss02", "ss03", "ss04", "ss05", "cv31"';
+        }
+
+        @define-color base   #${config.lib.stylix.colors.base00};
+        @define-color mantle #${config.lib.stylix.colors.base01};
+        @define-color crust  #11111b;
+
+        @define-color text     #${config.lib.stylix.colors.base05};
+        @define-color subtext0 #a6adc8;
+        @define-color subtext1 #bac2de;
+
+        @define-color surface0 #${config.lib.stylix.colors.base02};
+        @define-color surface1 #${config.lib.stylix.colors.base03};
+        @define-color surface2 #${config.lib.stylix.colors.base04};
+
+        @define-color overlay0 #6c7086;
+        @define-color overlay1 #7f849c;
+        @define-color overlay2 #9399b2;
+
+        @define-color blue      #${config.lib.stylix.colors.base0D};
+        @define-color lavender  #${config.lib.stylix.colors.base07};
+        @define-color sapphire  #74c7ec;
+        @define-color sky       #89dceb;
+        @define-color teal      #${config.lib.stylix.colors.base0C};
+        @define-color green     #${config.lib.stylix.colors.base0B};
+        @define-color yellow    #${config.lib.stylix.colors.base0A};
+        @define-color peach     #${config.lib.stylix.colors.base09};
+        @define-color maroon    #eba0ac;
+        @define-color red       #${config.lib.stylix.colors.base08};
+        @define-color mauve     #${config.lib.stylix.colors.base0E};
+        @define-color pink      #f5c2e7;
+        @define-color flamingo  #${config.lib.stylix.colors.base0F};
+        @define-color rosewater #${config.lib.stylix.colors.base06};
+
+        window#waybar {
+          	transition-property: background-color;
+          	transition-duration: 0.5s;
+          	background: transparent;
+          	border-radius: 10px;
+        }
+
+        window#waybar.hidden {
+          	opacity: 0.2;
+        }
+
+        window#waybar.empty,
+        window#waybar.empty #window {
+            background-color: transparent;
+            padding: 0px;
+            border: 0px;
+        }
+
+        #taskbar button,
+        #workspaces button {
+        	color: @overlay1;
+          box-shadow: none;
+        	text-shadow: none;
+          padding: 0px;
+          border-radius: 9px;
+          padding-left: 4px;
+          padding-right: 4px;
+          animation: gradient_f 20s ease-in infinite;
+          transition: all 0.5s cubic-bezier(.55,-0.68,.48,1.682);
+        }
+
+        #taskbar button:hover,
+        #workspaces button:hover {
+          border-radius: 10px;
+          color: @peach;
+          background-color: @surface0;
+          padding-left: 2px;
+          padding-right: 2px;
+          animation: gradient_f 20s ease-in infinite;
+          transition: all 0.3s cubic-bezier(.55,-0.68,.48,1.682);
+        }
+
+        #workspaces button.persistent {
+        	color: @surface1;
+        	border-radius: 10px;
+        }
+
+        #taskbar button.active,
+        #workspaces button.active {
+        	color: @mauve;
+          border-radius: 10px;
+          padding-left: 8px;
+          padding-right: 8px;
+          animation: gradient_f 20s ease-in infinite;
+          transition: all 0.3s cubic-bezier(.55,-0.68,.48,1.682);
+        }
+
+        #workspaces button.urgent {
+        	color: @red;
+         	border-radius: 0px;
+        }
+
+        .modules-center, .modules-right {
+          background: @base;
+          border: 0.5px solid @overlay0;
+        	padding-top: 2px;
+        	padding-bottom: 2px;
+        	padding-right: 4px;
+        	padding-left: 4px;
+        	border-radius: 10px;
+        }
+
+        .modules-right {
+          border: 1px solid @mauve;
+        }
+
+        #backlight,
+        #backlight-slider,
+        #battery,
+        #bluetooth,
+        #clock,
+        #cpu,
+        #disk,
+        #keyboard-state,
+        #memory,
+        #mode,
+        #mpris,
+        #network,
+        #power-profiles-daemon,
+        #pulseaudio,
+        #pulseaudio-slider,
+        #taskbar button,
+        #taskbar,
+        #temperature,
+        #tray,
+        #window,
+        #wireplumber,
+        #workspaces,
+        #custom-backlight,
+        #custom-browser,
+        #custom-cava_mviz,
+        #custom-cycle_wall,
+        #custom-dot_update,
+        #custom-file_manager,
+        #custom-keybinds,
+        #custom-keyboard,
+        #custom-light_dark,
+        #custom-lock,
+        #custom-hint,
+        #custom-hypridle,
+        #custom-menu,
+        #custom-playerctl,
+        #custom-power_vertical,
+        #custom-power,
+        #custom-quit,
+        #custom-reboot,
+        #custom-settings,
+        #custom-spotify,
+        #custom-swaync,
+        #custom-tty,
+        #custom-updater,
+        #custom-weather,
+        #custom-weather.clearNight,
+        #custom-weather.cloudyFoggyDay,
+        #custom-weather.cloudyFoggyNight,
+        #custom-weather.default,
+        #custom-weather.rainyDay,
+        #custom-weather.rainyNight,
+        #custom-weather.severe,
+        #custom-weather.showyIcyDay,
+        #custom-weather.snowyIcyNight,
+        #custom-weather.sunnyDay {
+        	padding-top: 4px;
+        	padding-bottom: 4px;
+        	padding-right: 6px;
+        	padding-left: 6px;
+        }
+
+        #idle_inhibitor {
+          color: @text;
+          padding-left: 4px;
+          padding-right: 4px;
+        }
+        #idle_inhibitor.activated {
+          color: @green;
+        }
+        #custom-hypridle.notactive,
+
+        #bluetooth,
+        #backlight {
+          color: @blue;
+        }
+
+        #battery {
+          color: @green;
+        }
+
+        @keyframes blink {
+          to {
+            color: @surface0;
+          }
+        }
+
+        #battery.critical:not(.charging) {
+          background-color: @red;
+          color: @text;
+        	animation-name: blink;
+        	animation-duration: 3.0s;
+        	animation-timing-function: steps(12);
+        	animation-iteration-count: infinite;
+        	animation-direction: alternate;
+          box-shadow: inset 0 -3px transparent;
+        }
+
+        #clock {
+          color: @yellow;
+        }
+
+        #cpu {
+          color: @green;
+        }
+
+        #custom-keyboard,
+        #memory {
+          color: @sky;
+        }
+
+        #disk {
+          color: @sapphire;
+        }
+
+        #temperature {
+          color: @teal;
+        }
+
+        #temperature.critical {
+          background-color: @red;
+        }
+
+        #tray > .passive {
+          -gtk-icon-effect: dim;
+        }
+        #tray > .needs-attention {
+          -gtk-icon-effect: highlight;
+        }
+
+        #keyboard-state {
+          color: @flamingo;
+        }
+
+        #custom-cava_mviz {
+        	color: @pink;
+        }
+
+        #custom-menu {
+          color: @rosewater;
+        }
+
+        #custom-power {
+          color: @red;
+        }
+
+        #custom-updater {
+          color: @red;
+        }
+
+        #custom-light_dark {
+          color: @blue;
+        }
+
+        #custom-weather {
+          color: @lavender;
+        }
+
+        #custom-lock {
+          color: @maroon;
+        }
+
+        #pulseaudio {
+          color: @sapphire;
+        }
+
+        #pulseaudio.bluetooth {
+          color: @pink;
+        }
+        #pulseaudio.muted {
+          color: @red;
+        }
+
+        #window {
+          color: @mauve;
+        }
+
+        #mpris {
+          color:@lavender;
+        }
+
+        #network {
+          color: @teal;
+        }
+        #network.disconnected,
+        #network.disabled {
+          background-color: @surface0;
+          color: @text;
+        }
+
+        #backlight-slider slider,
+        #pulseaudio-slider slider {
+        	min-width: 0px;
+        	min-height: 0px;
+        	opacity: 0;
+        	background-image: none;
+        	border: none;
+        	box-shadow: none;
+        }
+
+        #backlight-slider trough,
+        #pulseaudio-slider trough {
+        	min-width: 80px;
+        	min-height: 5px;
+        	border-radius: 5px;
+        }
+
+        #backlight-slider highlight,
+        #pulseaudio-slider highlight {
+        	min-height: 10px;
+        	border-radius: 5px;
+        }
+      ''
+    ];
   };
 }
