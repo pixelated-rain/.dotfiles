0ccaacb1b55bd4c554e936ec1e96aa006446ca69
CharredLee <gcc2hq@virginia.edu>
2025-04-21 22:08:41 -0400

    replace git email with noreply email

--- hosts/laptop2/variables.nix
+++ hosts/laptop2/variables.nix
@@ -1,7 +1,7 @@
 {
   # Git Configuration ( For Pulling Software Repos )
   gitUsername = "CharredLee";
-  gitEmail = "gcc2hq@virginia.edu";
+  gitEmail = "31701136+CharredLee@users.noreply.github.com";
 
   # Hyprland Settings
   extraMonitorSettings = "
