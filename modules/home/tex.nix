0e85f5d696d2e5fde22c88e8955af2b8eb2ba1e1
658060 <31701136+658060@users.noreply.github.com>
2026-05-05 23:18:11 -0400

    remove custom global tex packages
    they weren't working anyway

--- modules/home/tex.nix
+++ modules/home/tex.nix
@@ -5,18 +5,8 @@
   ...
 }: let
   inherit (config.variables) texlive;
-  latex-custom-packages = pkgs.stdenvNoCC.mkDerivation {
-    name = "latex-custom-packages";
-    src = ../../texmf;
-    installPhase = "cp -r $src $out";
-    passthru.tlType = "run";
-  };
-  texlive-custom-packages = {
-    pkgs = [latex-custom-packages];
-  };
   tex = pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-full;
-    inherit texlive-custom-packages;
   };
 in {
   home.packages = lib.mkIf texlive [
