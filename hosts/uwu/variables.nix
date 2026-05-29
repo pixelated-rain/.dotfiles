48293f329f32ca801dd24aa69d9f83ff881d5e8e
pixelated-rain <dreamsofsoftrain@gmail.com>
2026-05-28 21:04:04 -0400

    resolution fix

--- hosts/uwu/variables.nix
+++ hosts/uwu/variables.nix
@@ -5,7 +5,7 @@
 
   # Hyprland Settings
   extraMonitorSettings = "
-    monitor = DP-3,3840x2160@60.0Hz,0x0,2.0
+    monitor = DP-3,3840x2160@60.0Hz,0x0,1.0
     monitor = eDP-1,1920x1080@60.00Hz,auto-right,1.0
   ";
   extraHardwareSettings = "
